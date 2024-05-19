f5553f83d868d8517207c9e57c0886d4f67338a6
khelan-mehta <khelan05@gmail.com>
2024-05-19 17:02:15 +0530

    maybe this is it

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,7 @@ import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(UsersModule);
-  app.enableCors();
   app.use(json({ limit: '50mb' }));
-  app.use(urlencoded({ extended: true, limit: '500mb' }));
-  console.log('');
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
 }
